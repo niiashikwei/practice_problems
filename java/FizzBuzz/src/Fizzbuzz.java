d8c52eb8e962d7b5b12f52c9fa031663b52b2443
Nii <niiashikwei@hotmail.com>
2014-07-06 18:34:04 -0500

    Nii - refactor step one done

--- java/FizzBuzz/src/Fizzbuzz.java
+++ java/FizzBuzz/src/Fizzbuzz.java
@@ -1,13 +1,18 @@
+import java.io.PrintStream;
+
 public class Fizzbuzz {
 
-
     public static void main(String args[]){
+        fizzBuzz(System.out);
+    }
+
+    protected static void fizzBuzz(PrintStream printStream) {
         for (int i = 1; i < 101; i++){
-            System.out.printf("%s \n", fizzBuzzInterpreter(i));
+            printStream.printf("%s \n", fizzBuzzInterpreter(i));
         }
     }
 
-    public static String fizzBuzzInterpreter(Integer numberToPrint) {
+    protected static String fizzBuzzInterpreter(Integer numberToPrint) {
         boolean isMultipleOfFive = numberToPrint % 5 == 0;
         boolean isMultipleOfThree = numberToPrint % 3 == 0;
 
