d5331515c9e0a51887c3e5a6f87120f51b054d0a
Nii <niiashikwei@hotmail.com>
2014-07-09 11:23:06 -0500

    NT VP test case when 4

--- java/PrimeFactorsKata/src/PrimeFactors.java
+++ java/PrimeFactorsKata/src/PrimeFactors.java
@@ -1,9 +1,14 @@
 
 public class PrimeFactors {
     protected static int[] getPrimeFactors(int number){
-        if (number == 2){
-            return new int[]{2};
+        if (number == 2 || number == 3){
+            return new int[]{number};
         }
+
+        if (number == 4){
+            return new int[]{2,2};
+        }
+
         return new int[]{};
     }
 }
--- java/PrimeFactorsKata/test/PrimeFactorsKata.java
+++ java/PrimeFactorsKata/test/PrimeFactorsKata.java
@@ -2,6 +2,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
+//
+//0 = []
+//        1 = []
+//        2 = [2]
+//        3 = [3]
+//        4 = 2, 2
+// prime numbers
+// remove prime numbers that dont divide exactly
 
 public class PrimeFactorsKata {
     @Test
@@ -20,5 +28,17 @@ public class PrimeFactorsKata {
     public void shouldGetPrimeFactorsForTwo(){
         int[] expectedResult = new int[]{2};
         assertArrayEquals(expectedResult, PrimeFactors.getPrimeFactors(2));
+    }
+
+    @Test
+    public void shouldGetPrimeFactorsForThree(){
+        int[] expectedResult = new int[]{3};
+        assertArrayEquals(expectedResult, PrimeFactors.getPrimeFactors(3));
+    }
+
+    @Test
+    public void shouldGetPrimeFactorsForFour(){
+        int[] expectedResult = new int[]{2,2};
+        assertArrayEquals(expectedResult, PrimeFactors.getPrimeFactors(4));
     }
 }
