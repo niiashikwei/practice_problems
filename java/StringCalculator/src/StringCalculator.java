561c196b2435c1b07920a637cd7755ee08ad05a1
Nii <niiashikwei@hotmail.com>
2014-07-10 10:51:15 -0500

    String Calculator Kata, refactored implementation and tests

--- java/StringCalculator/src/StringCalculator.java
+++ java/StringCalculator/src/StringCalculator.java
@@ -1,26 +1,13 @@
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
-
 public class StringCalculator {
 
-    public List<Integer> convertString(String inputNumber) {
-        List<Integer> result = newArrayList();
-        if(!inputNumber.isEmpty()){
-            String[] charArray = inputNumber.split(",");
+    public int sum(String inputString) {
+        int sum = 0;
+        if(!inputString.isEmpty()){
+            String[] charArray = inputString.split(",");
             for(int index = 0; index < charArray.length; index++){
                 String stringNumber = charArray[index].trim();
-                result.add(Integer.parseInt(stringNumber));
+                sum += Integer.parseInt(stringNumber);
             }
-        }
-        return result;
-    }
-
-    public int sum(String inputString) {
-        List<Integer> integers = convertString(inputString);
-        int sum = 0;
-        for(int integer : integers){
-            sum += integer;
         }
         return sum;
     }
--- java/StringCalculator/test/StringCalculatorTest.java
+++ java/StringCalculator/test/StringCalculatorTest.java
@@ -1,38 +1,8 @@
 import org.junit.Test;
 
-import java.util.List;
-
-import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
-    @Test
-    public void shouldParseEmptyStringAndReturnEmptyArray(){
-        StringCalculator stringCalculator = new StringCalculator();
-        List<Integer> actualArray = stringCalculator.convertString("");
-        List<Integer> expectedArray = newArrayList();
-
-        assertEquals(expectedArray, actualArray);
-    }
-
-    @Test
-    public void shouldParseAStringThreeAndReturnIntegerThree(){
-        StringCalculator stringCalculator = new StringCalculator();
-        List<Integer> actualArray = stringCalculator.convertString("3");
-        List<Integer> expectedArray = newArrayList(3);
-
-        assertEquals(expectedArray, actualArray);
-    }
-
-    @Test
-    public void shouldParseStringWithTwoNumbersAndReturnArrayOfTwoIntegers(){
-        StringCalculator stringCalculator = new StringCalculator();
-        List<Integer> actualArray = stringCalculator.convertString("1, 2");
-        List<Integer> expectedResult = newArrayList(1,2);
-
-        assertEquals(expectedResult, actualArray);
-    }
-
     @Test
     public void shouldSumNoNumbers(){
         StringCalculator stringCalculator = new StringCalculator();
